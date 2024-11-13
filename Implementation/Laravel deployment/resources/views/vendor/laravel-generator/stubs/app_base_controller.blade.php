e11f472b5a1246dfd2a94c5510c4c49097a70d27
MuchiriKinyua <muchirikinyua2000@gmail.com>
2024-11-13 03:17:43 +0300

    correcting errors

--- Implementation/Laravel deployment/resources/views/vendor/laravel-generator/stubs/app_base_controller.blade.php
+++ Implementation/Laravel deployment/resources/views/vendor/laravel-generator/stubs/app_base_controller.blade.php
@@ -1,8 +1,6 @@
-@php
-    echo "<?php".PHP_EOL;
-@endphp
+<?php
 
-namespace {{ $namespaceApp }}Http\Controllers;
+namespace App\Http\Controllers;
 
 use InfyOm\Generator\Utils\ResponseUtil;
 
@@ -12,21 +10,42 @@ use InfyOm\Generator\Utils\ResponseUtil;
  *   title="InfyOm Laravel Generator APIs",
  *   version="1.0.0"
  * )
- * This class should be parent class for other API controllers
+ * This class should be the parent class for other API controllers.
+ * 
  * Class AppBaseController
  */
 class AppBaseController extends Controller
 {
+    /**
+     * Send a success response
+     *
+     * @param mixed $result
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function sendResponse($result, $message)
     {
         return response()->json(ResponseUtil::makeResponse($message, $result));
     }
 
+    /**
+     * Send an error response
+     *
+     * @param string $error
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function sendError($error, $code = 404)
     {
         return response()->json(ResponseUtil::makeError($error), $code);
     }
 
+    /**
+     * Send a success message
+     *
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function sendSuccess($message)
     {
         return response()->json([
